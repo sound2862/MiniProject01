24b5f12d4cf3c093b0fb3ba29483a9f64977eef8
Myname-Jonghee <jonghee4911@naver.com>
2024-08-07 17:34:36 +0900

    옵션 선택 창

---
+++ 이종희/AddOption.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MiniProjectBuycar
+{
+    public partial class AddOption : Form
+    {
+        public AddOption()
+        {
+            InitializeComponent();
+            comboBox1.Items.Add("스탠다드");
+            comboBox1.Items.Add("노블레스");
+            comboBox1.Items.Add("시그니처");
+
+            comboBox1.SelectedIndexChanged += new EventHandler(comboBox1_SelectedIndexChanged);
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem.ToString() == "스탠다드")
+            {
+                textBox1.Text = "스탠다드옵션 ->버튼시동,크루즈컨트롤,17인치 휠 타이어,전자식 파킹브레이크를 제공합니다.";
+            }
+            else if(comboBox1.SelectedItem.ToString() =="노블레스")
+            {
+                textBox1.Text = "노블레스옵션 -> 스탠다드 + 가죽시트 + 서라운드 뷰을 제공합니다.";
+            }
+            else if(comboBox1.SelectedItem.ToString() == "시그니처")
+            {
+                textBox1.Text = "시그니처옵션 -> 노블레스 + HUD + 파노라마 썬루프를 제공합니다.";
+            }
+        }
+    }
+}
